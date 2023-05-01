b87c349deb953a28a622d0863ce4fb7814e575b5
martin <martinalejandronuniez@gmail.com>
2023-04-30 21:16:12 -0300

    quinto commit

--- js/app.js
+++ js/app.js
@@ -79,6 +79,25 @@ productList.addEventListener('click', e => {
         //  console.log(infoProduct)
         //vemos si nos da bien la informacion
 
+
+        const exists = allProducts.some( producto => producto.titulo === infoProduct.titulo)
+        //con el metodo .some podemos saber si un valor esta dentro del arreglo
+        //entonces producto sera una variable del metodo donde se va a alojar un producto
+        //del arreglo allProducts, por cada iteracion. y se va a comparar especificamente
+        //si el titulo de ese producto coincide con el producto que el usuario hizo click
+        //recordemos que cuando el usuario hace click en un producto, se guardan los datos
+        //de cantidad, titulo y precio, en el arreglo infoProduct
+
+        //este metodo recorrera todo el arreglo donde estan todos los productos del carrito
+        //y los comparara uno por uno con el titulo del producto que el usuario toco
+        //el boton de añadir al carrito, si ya existe ese titulo en el arreglo allProducts
+        //la funcion devolvera verdadero, y sino falso. y a ese resultado lo guardara en la 
+        //constante exists
+
+        //podemos comprobar esto haciendo: console.log(exists) e ir tocando los botones
+        //y fijandose en consola si se repiten
+
+        
         allProducts = [...allProducts, infoProduct]
         //con el operador "..." spread, lo que hace es hacer un nuevo arreglo, esparce cada elemento
         //que tenia en la copia original, quedando igual al arreglo original, y al final
@@ -89,15 +108,19 @@ productList.addEventListener('click', e => {
         //lo que ya teniamos en el arreglo original (allproducts
 
         console.log(allProducts)
+        showHTML();
     }
 })
 
 // Funcion para mostrar HTML
 
 const showHTML = () =>{
-    allProducts.forEach(product =>{ //el forEach recorre todo el arreglo, 
+    // Limpiar HTML de lo que tenia antes en la lista de productos seleccionados:
+    rowProduct.innerHTML = " ";
+
+    allProducts.forEach(producto =>{ //el forEach recorre todo el arreglo, 
         //y por cada uno de los campos... ejecuta la funcion que hay adentro,
-        //product va a ser donde se va a guardar cada campo del arreglo,
+        //producto va a ser donde se va a guardar cada campo del arreglo,
         //mientras se lo este recorriendo  
 
     const containerProduct = document.createElement('div')
@@ -107,7 +130,50 @@ const showHTML = () =>{
     containerProduct.classList.add('cart-product')
     //aca le añadimos a ese <div> la clase cart-product.
         
+    containerProduct.innerHTML =    
+    // con innerHTML. lo insertamos en el documento html
+                                    
+    //Hacemos la plantilla de lo que vamos a insertar
+    //y reemplazamos en cantidad, titulo del producto y precio
+    //por la variable producto.cantidad, producto.titulo y producto.precio que salen
+    //de cada iteracion del foreach, que recorre el arreglo allProducts
+    `                                      
+        <div class="info-cart-product">
+            <span class="cantidad-producto-carrito">${producto.cantidad}</span> 
+            <p class="titulo-producto-carrito">${producto.titulo}</p>
+            <span class="precio-producto-carrito">${producto.precio}</span>
+        </div>
+        <!-- icono de X -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24     24"     stroke-width="1.5" stroke="currentColor" class="icon-close">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18        6M6 6l12 12" />
+        </svg>
+    `
+    /*hasta aca lo que hicimos, es que una vez que entra al forEach del Arreglo allProducts,  
+    donde estan todos los productos que el usuario selecciono..
+    por cada iteracion del forEach lo que hace es crear una constante 
+    en donde vamos a guardar todo el codigo que vamos a insertar.
+    primero crea la constante, creando en ella un elemento div, luego le agrega a ese div
+    la clase "cart-product", que es la misma que tiene el documento html original
+    y finalmente inserta dentro de ese contenedor div, los datos que proporcionamos en
+    la plantilla que hicimos, quedando listo todo el contenido que vamos a insertar
+    sobre ese producto en particular, en esa iteracion.
+    queda todo listo guardado en la constante containerProduct
+    */
+    
 
+    
+    rowProduct.append(containerProduct)
+    //aca insertamos el codigo que hicimos en el <div clase="row-product">
+    //del documento html
+
+    /*quedando: 
+            <div clase="row-product">
+                <div clase="cart-product">
+                    Producto insertado
+                </div>
+            </div> 
+    */
+    
     }) 
 } 
 
